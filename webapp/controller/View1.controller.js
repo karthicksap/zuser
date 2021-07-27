c7a51e9df8c7c7fef7f8097626d022be8b60d8c0
karthick ramasamy <nameiskarthick@gmail.com>
2021-07-27 12:18:32 +0000

    third change from BAS

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,209 +1,237 @@
 sap.ui.define([
-	"art/bif/ur/controller/BaseController",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/Fragment",
-	"art/bif/ur/util/Formatter",
-	"sap/m/MessagePopover"
-], function(BaseController, JSONModel, Fragment, Formatter, MessagePopover) {
+    "art/bif/ur/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment",
+    "art/bif/ur/util/Formatter",
+    "sap/m/MessagePopover",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast",
+    "sap/ui/core/routing/History"
+], function (BaseController, JSONModel, Fragment, Formatter, MessagePopover, MessageBox,
+    MessageToast, History) {
 
-	return BaseController.extend("art.bif.ur.controller.View1", {
+    return BaseController.extend("art.bif.ur.controller.View1", {
 
-		onInit: function() {
+        onInit: function () {
 
-			this.oLocal = new JSONModel({
+            this.oLocal = new JSONModel({
 
-				"supplier": {
-					"SupplierNo": "",
-					"Name": "",
-					"PanNo": "",
-					"Building": "",
-					"Street": "",
-					"City": "",
-					"District": "",
-					"PostalCode": "",
-					"Region": ""
+                "supplier": {
+                    "SUPPLIE_NO": "",
+                    "NAME": "",
+                    "PAN_NO": "",
+                    "BUILDING": "",
+                    "STREET": "",
+                    "CITY": "",
+                    "DISTRICT": "",
+                    "POSTAL_CODE": "",
+                    "REGION": ""
 
-				}
+                }
 
-			});
+            });
 
-			this.getView().setModel(this.oLocal, "data");
+            this.getView().setModel(this.oLocal, "data");
 
-		},
+        },
 
-		// onPopPress: function(oEvent) {
+        // onPopPress: function(oEvent) {
 
-		// 	if (!this.oMP) {
-				
-		// 		this.createMessagePopover();
-		// 	}
-		// 	this.oMP.toggle(oEvent.getSource());
+        // 	if (!this.oMP) {
 
-		// },
+        // 		this.createMessagePopover();
+        // 	}
+        // 	this.oMP.toggle(oEvent.getSource());
 
-		fragmentfin: null,
+        // },
 
-		oFormatter: Formatter,
+        fragmentfin: null,
 
-		onHelp: function(oEvent) {
+        oFormatter: Formatter,
 
-			this.oRegion = oEvent.getSource();
+        onHelp: function (oEvent) {
 
-			if (!this.fragmentfin) {
+            this.oRegion = oEvent.getSource();
 
-				Fragment.load({
+            if (!this.fragmentfin) {
 
-					name: "art.bif.ur.fragments.popup",
-					id: "idState",
-					controller: this
+                Fragment.load({
 
-				}).then(this.loadState.bind(this));
+                    name: "art.bif.ur.fragments.popup",
+                    id: "idState",
+                    controller: this
 
-			} else {
+                }).then(this.loadState.bind(this));
 
-				this.fragmentfin.open();
+            } else {
 
-			}
+                this.fragmentfin.open();
 
-		},
-		loadState: function(oFragment) {
+            }
 
-			var oDataModel = this.getView().getModel();
+        },
+        loadState: function (oFragment) {
 
-			this.fragmentfin = oFragment;
+            var oDataModel = this.getView().getModel();
 
-			this.getView().addDependent(this.fragmentfin);
+            this.fragmentfin = oFragment;
 
-			this.fragmentfin.bindAggregation("items", {
+            this.getView().addDependent(this.fragmentfin);
 
-				path: "/ZshStateSet",
-				template: new sap.m.StandardListItem({
+            this.fragmentfin.bindAggregation("items", {
 
-					title: "{Bland}",
-					description: "{Bezei}"
+                path: "/ZshStateSet",
+                template: new sap.m.StandardListItem({
 
-				})
+                    title: "{Bland}",
+                    description: "{Bezei}"
 
-			});
+                })
 
-			this.fragmentfin.open();
+            });
 
-		},
-		onSave: function() {
-	
-			var aMessages = [];
-			
-			if (!this.getView().byId("idName").getValue()) {
+            this.fragmentfin.open();
 
-				this.getView().byId("idName").setValueState(sap.ui.core.ValueState.Error);
-				
-			var	oMessage = {
-					
-				type: 'Error',
-				title: 'Error message',
-				active: true,
-				description: 'Name should not be empty',
-				subtitle: 'Example of subtitle',
-				counter: 1
-				};
-				
-				aMessages.push(oMessage);
+        },
+        onSave: function () {
 
-			} else {
+            if (!this.getView().byId("idName").getValue()) {
 
-				this.getView().byId("idName").setValueState(sap.ui.core.ValueState.None);
+                this.getView().byId("idName").setValueState(sap.ui.core.ValueState.Error);
 
-			}
+            } else {
 
-			if (!this.getView().byId("idPan").getValue()) {
+                this.getView().byId("idName").setValueState(sap.ui.core.ValueState.None);
 
-				this.getView().byId("idPan").setValueState(sap.ui.core.ValueState.Error);
+            }
 
-			} else {
+            if (!this.getView().byId("idPan").getValue()) {
 
-				this.getView().byId("idPan").setValueState(sap.ui.core.ValueState.None);
+                this.getView().byId("idPan").setValueState(sap.ui.core.ValueState.Error);
 
-			}
+            } else {
 
-			if (!this.getView().byId("idStreet").getValue()) {
+                this.getView().byId("idPan").setValueState(sap.ui.core.ValueState.None);
 
-				this.getView().byId("idStreet").setValueState(sap.ui.core.ValueState.Error);
+            }
 
-			} else {
+            if (!this.getView().byId("idStreet").getValue()) {
 
-				this.getView().byId("idStreet").setValueState(sap.ui.core.ValueState.None);
+                this.getView().byId("idStreet").setValueState(sap.ui.core.ValueState.Error);
 
-			}
+            } else {
 
-			if (!this.getView().byId("idBuilding").getValue()) {
+                this.getView().byId("idStreet").setValueState(sap.ui.core.ValueState.None);
 
-				this.getView().byId("idBuilding").setValueState(sap.ui.core.ValueState.Error);
+            }
 
-			} else {
+            if (!this.getView().byId("idBuilding").getValue()) {
 
-				this.getView().byId("idBuilding").setValueState(sap.ui.core.ValueState.None);
+                this.getView().byId("idBuilding").setValueState(sap.ui.core.ValueState.Error);
 
-			}
+            } else {
 
-			if (!this.getView().byId("idCity").getValue()) {
+                this.getView().byId("idBuilding").setValueState(sap.ui.core.ValueState.None);
 
-				this.getView().byId("idCity").setValueState(sap.ui.core.ValueState.Error);
+            }
 
-			} else {
+            if (!this.getView().byId("idCity").getValue()) {
 
-				this.getView().byId("idCity").setValueState(sap.ui.core.ValueState.None);
+                this.getView().byId("idCity").setValueState(sap.ui.core.ValueState.Error);
 
-			}
+            } else {
 
-			if (!this.getView().byId("idPost").getValue()) {
+                this.getView().byId("idCity").setValueState(sap.ui.core.ValueState.None);
 
-				this.getView().byId("idPost").setValueState(sap.ui.core.ValueState.Error);
+            }
 
-			} else {
+            if (!this.getView().byId("idPost").getValue()) {
 
-				this.getView().byId("idPost").setValueState(sap.ui.core.ValueState.None);
+                this.getView().byId("idPost").setValueState(sap.ui.core.ValueState.Error);
 
-			}
+            } else {
 
-			if (!this.getView().byId("idDis").getValue()) {
+                this.getView().byId("idPost").setValueState(sap.ui.core.ValueState.None);
 
-				this.getView().byId("idDis").setValueState(sap.ui.core.ValueState.Error);
+            }
 
-			} else {
+            if (!this.getView().byId("idDis").getValue()) {
 
-				this.getView().byId("idDis").setValueState(sap.ui.core.ValueState.None);
+                this.getView().byId("idDis").setValueState(sap.ui.core.ValueState.Error);
 
-			}
+            } else {
 
-			if (!this.getView().byId("idReg").getValue()) {
+                this.getView().byId("idDis").setValueState(sap.ui.core.ValueState.None);
 
-				this.getView().byId("idReg").setValueState(sap.ui.core.ValueState.Error);
+            }
 
-			} else {
+            if (!this.getView().byId("idReg").getValue()) {
 
-				this.getView().byId("idReg").setValueState(sap.ui.core.ValueState.None);
+                this.getView().byId("idReg").setValueState(sap.ui.core.ValueState.Error);
 
-			}
+            } else {
 
-			var oDataModel = this.getView().getModel();
+                this.getView().byId("idReg").setValueState(sap.ui.core.ValueState.None);
 
-			var that = this;
+            }
 
-			oDataModel.callFunction("/GetSupplierNo", {
 
-				success: function(data) {
+            var that = this;
 
-					that.oLocal.setProperty("/supplier/SupplierNo", data.GetSupplierNo.SupplierNo);
+            MessageBox.confirm("Please check information before save once it's saved you won't be able to change you need contact admin to change", {
 
-				}
-			});
-		},
-		onConfirm: function(oEvent) {
+                title: "Do you want to save it?",
+                onClose: this.messConfirm.bind(this)
 
-			this.oRegion.setValue(oEvent.getParameter("selectedItem").getProperty("title"));
+            });
 
-		}
+            var oDataModel = this.getView().getModel();
 
-	});
+            oDataModel.callFunction("/GetSupplierNo", {
+
+                success: function (data) {
+                    debugger;
+                    that.oLocal.setProperty("/supplier/SUPPLIE_NO", data.GetSupplierNo.SupplierNo);
+
+                }
+            });
+        },
+        onConfirm: function (oEvent) {
+
+            this.oRegion.setValue(oEvent.getParameter("selectedItem").getProperty("title"));
+
+        },
+
+        messConfirm: function (result) {
+
+            if (result === "OK") {
+
+                var oDataModel = this.getView().getModel();
+
+                var payLoad = this.getView().getModel("data").getProperty("/supplier");
+                debugger;
+                oDataModel.create("/UserSet", payLoad, {
+
+                    success: this.afterSave.bind(this),
+                    error: this.afterFail
+
+                });
+
+            }
+
+        },
+
+        afterSave: function (oSucc) {
+
+            MessageToast.show("User registred successfully");
+            this.getView().getModel("data").setProperty("/supplier", "");
+            location.reload();
+
+        },
+
+        afterFail: function (oFail) {
+
+        }
+    });
 
 });
